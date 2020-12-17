946009b30c9e4d56beb61ce13d098bf9a25a499a
Viviana Garcia <vivianandrea4@gmail.com>
2020-12-17 10:09:34 -0500

    no serious commits

--- src/ProfilePage/ProfilePage.js
+++ src/ProfilePage/ProfilePage.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import axios from 'axios';
 
 const ProfilePage = () => {
+
+    // const [users, setUsers] = useState([])
+
+    // useEffect(() => {
+    //     axios
+    //         .get(get all users)
+    //         .then((response){
+    //             setUsers(response);
+    //         })
+    // })
+
+    // const FoodBoxes = (food) => {
+    //     return users.map((eachFood) => {
+    //         return <FoodBox {...eachFood} />;
+    //     });
+    // };
+
+    // const searchFriend = (event) => {
+    //     console.log(event.target.value);
+    //     let filteredFoods = allFoods.filter((eachFood) => {
+    //         return eachFood.name
+    //             .toUpperCase()
+    //             .includes(event.target.value.toUpperCase());
+    //     });
+    //     setShowFoods(filteredFoods);
+    // };
+
     return (
         <div>
-            <h1>this is my profile page</h1>
+
+            {/* <form>
+                <input onChange={searchFriend} type="text" placeholder="Search Here" />
+            </form> */}
+
+            <div>
+                //Show users
+        </div>
+
 
         </div>
     );
